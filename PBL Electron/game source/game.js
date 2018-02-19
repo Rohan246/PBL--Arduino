4b0b9373e14abbedb909c0a39b9ad64f40062acf
Rohan Sugggala <Rohan@Rohans-Mac-mini.local>
2018-02-19 11:26:18 -0500

    7th centurey

--- PBL Electron/game source/game.js
+++ PBL Electron/game source/game.js
@@ -1,13 +1,17 @@
 var canvas
 var context
 
-var controller = require("controller.js")
+var controller = require("./game source/controller.js")
+
+var x = 1
 
 function initFrame()
 {
   window.setInterval(() => // Frame update function
   {
     // Put ur code here
+    context.drawImage(loadImage("./game source/images/s.jpg"), x, 50, 50, 50)
+
   }, 20)
 }
 
@@ -23,6 +27,13 @@ window.onload = () => // Setup
 {
   canvas = document.getElementById("game")
   context = canvas.getContext("2d")
+
+  controller.initializeEventScheduler()
+
+  controller.on("stickRight", () => 
+  {
+    x = x + 10 
+  })
 
   initFrame()
 }
