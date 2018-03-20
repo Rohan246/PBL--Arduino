4efc04c83c33285630a278048530f64fb407f206
GreenCommunications <alexander.m.rankin@gmail.com>
2018-03-20 00:23:24 -0400

    Final
    Hope to never see this again... ._.

--- PBL Electron/game source/game.js
+++ PBL Electron/game source/game.js
@@ -6,21 +6,31 @@ var controller = require("./game source/controller.js")
 var x = 800
 var y = 0
 
+var portalThreshold = 1360
+
 // Place loadImage instances here
 var charStageOne = loadImage("./game source/images/charactermodel/sprite1.png")
 var charStageTwo = loadImage("./game source/images/charactermodel/sprite2.png")
 var charStageThree = loadImage("./game source/images/charactermodel/sprite3.png")
+
+var background = loadImage("./game source/images/bg.jpg")
+
+var portal = loadImage("./game source/images/portal.png")
 
 function initFrame()
 {
   window.setInterval(() => // Frame update function
   {
     // Put ur code here
-    context.drawImage(loadImage("./game source/images/bg.jpg"), 1, 1, window.innerWidth, window.innerHeight)
-    context.drawImage(loadImage("./game source/images/portal.png"), window.innerWidth - 200, 450, 150, 150)
+    context.drawImage(background, 1, 1, window.innerWidth, window.innerHeight)
+    context.drawImage(portal, window.innerWidth - 200, 450, 150, 150)
     context.drawImage(charStageOne, x, 540, 28, 53)
-    console.log(x)
-    console.log(y)
+
+    if(x >= portalThreshold)
+    {
+      document.getElementById("game").style.display = "none";
+      document.getElementById("quiz").style.display = "block";
+    }
   }, 1)
 }
 
@@ -72,4 +82,53 @@ window.onload = () => // Setup
   // End window control code:
 
   initFrame()
+}
+
+function quizCheck()
+{
+  document.getElementById("scorecard").style.display = "block";
+  var correct = 0;
+
+  var s1 = document.getElementById("1").checked;
+  var s2 = document.getElementById("2").checked;
+  var s3 = document.getElementById("3").checked;
+  var s4 = document.getElementById("4").checked;
+
+  if(s1)
+  {
+    correct--;
+  }
+  else
+  {
+    correct++;
+  }
+
+  if(s2)
+  {
+    correct++;
+  }
+  else
+  {
+    correct--;
+  }
+
+  if(s3)
+  {
+    correct++;
+  }
+  else
+  {
+    correct--;
+  }
+
+  if(s4)
+  {
+    correct++;
+  }
+  else
+  {
+    correct--;
+  }
+
+  document.getElementById("percent").innerHTML = "100%";
 }
